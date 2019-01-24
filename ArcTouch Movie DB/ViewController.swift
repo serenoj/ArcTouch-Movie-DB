6229e48d6909f5732aa6b818fb01e19df5dbb717
Juan Correa Arango - iOS <juanetas@icloud.com>
2019-01-24 05:39:37 -0500

    Initial Commit

---
+++ ArcTouch Movie DB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArcTouch Movie DB
+//
+//  Created by Juan Carlos Correa Arango on 1/24/19.
+//  Copyright © 2019 ArcTouch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
